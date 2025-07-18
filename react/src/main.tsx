91ee7520a45a18e07d8ead4ff429458ff7512a36
ram0973 <ram0973@gmail.com>
2025-07-18 15:17:01 +0300

    add Query provider

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,12 +1,22 @@
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
 // Import the generated route tree
+import * as TanStackQueryProvider from './root-provider.tsx'
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  context: {
+    ...TanStackQueryProvider.getContext(),
+  },
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+  defaultStructuralSharing: true,
+  defaultPreloadStaleTime: 0,
+})
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -21,7 +31,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <TanStackQueryProvider.Provider>
+        <RouterProvider router={router} />
+      </TanStackQueryProvider.Provider>
     </StrictMode>,
   );
 }
