67a3bf98edf9591b3c8b31cdbb7e79428583decb
ram0973 <ram0973@gmail.com>
2025-07-18 15:15:36 +0300

    add User type for dashboard

---
+++ react/src/types/User.ts
@@ -0,0 +1,14 @@
+export type User = {
+  id: string,
+  email: string,
+  firstName: string,
+  lastName: string,
+  avatar: string,
+  password: string,
+  enabled: boolean,
+  roles: [],
+  createdDate: string,
+  lastModifiedDate: string,
+  createdBy: string,
+  lastModifiedBy: string,
+}
