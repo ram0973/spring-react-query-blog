67fc4338489a05c823c44d0073e2dea131c919a7
ram0973 <ram0973@gmail.com>
2025-07-18 07:57:58 +0300

    add react query

--- react/src/routes/dashboard/index.tsx
+++ react/src/routes/dashboard/index.tsx
@@ -1,7 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+const usersQueryOptions = () =>
+	queryOptions({
+		queryKey: ['users'],
+		queryFn: getUsers,
+	})
+
+
 export const Route = createFileRoute("/dashboard/")({
   component: RouteComponent,
+  loader: async ({ context }) => {
+		await context.queryClient.ensureQueryData(usersQueryOptions())
+	},
+	head: () => ({
+		meta: [{ title: 'Dashboard | Users' }],
+	}),
+
 });
 
 function RouteComponent() {
